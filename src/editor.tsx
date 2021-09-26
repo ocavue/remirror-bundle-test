f73a84887e3b64f39a06f6d5104dfcd6e5a8a2b2
ocavue <ocavue@gmail.com>
2021-09-27 07:12:41 +0800

    import EmojiExtension

--- src/editor.tsx
+++ src/editor.tsx
@@ -2,11 +2,15 @@ import "@remirror/styles/all.css";
 
 import { Remirror, ThemeProvider, useRemirror } from "@remirror/react";
 import React from "react";
-import { ItalicExtension } from "remirror/extensions";
+import { ItalicExtension, EmojiExtension } from "remirror/extensions";
+import emojiData from "svgmoji/emoji.json";
 
 const Editor: React.FC = () => {
   const { manager } = useRemirror({
-    extensions: () => [new ItalicExtension()],
+    extensions: () => [
+      new ItalicExtension(),
+      new EmojiExtension({ plainText: true, data: emojiData }),
+    ],
     content: "<p>Some text</p>",
     stringHandler: "html",
   });
