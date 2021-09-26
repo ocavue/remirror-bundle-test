37a7845beae4ebd96e6cae4cb2d64be3378544f0
ocavue <ocavue@gmail.com>
2021-09-27 07:08:32 +0800

    import ItalicExtension

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,11 +1,12 @@
-import '@remirror/styles/all.css';
+import "@remirror/styles/all.css";
 
 import { Remirror, ThemeProvider, useRemirror } from "@remirror/react";
 import React from "react";
+import { ItalicExtension } from "remirror/extensions";
 
 const Editor: React.FC = () => {
   const { manager } = useRemirror({
-    extensions: () => [],
+    extensions: () => [new ItalicExtension()],
     content: "<p>Some text</p>",
     stringHandler: "html",
   });
